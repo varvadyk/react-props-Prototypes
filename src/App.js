874b7a42e79054e1ca1829fdb548848f71c714f9
Diana <diana_ivanivna@kr.net>
2020-07-04 18:16:20 +0300

    working with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './Header';
 import './App.css';
 
-function App() {
+const menu=[
+  {
+    link:'/articles',
+    label:'Article'
+  },
+  {
+    link:'/contacts',
+    label:'Contacts'
+  },
+  {
+    link:'posts',
+    label:"Posts"
+  }
+];
+ class App extends Component {
+   render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header  items={menu} />
     </div>
   );
 }
+ }
 
 export default App;
